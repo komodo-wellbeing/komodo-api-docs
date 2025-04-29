2d9c8167f1a72a740d1e41306aefffdd4e895a62
Zach Lang <zach.lang@komodowellbeing.com>
2025-04-29 15:58:52 +1200

    Adds empty Power BI integration guide page

---
+++ website/docs/guides/sidebar.ts
@@ -0,0 +1,7 @@
+import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
+
+const sidebar: SidebarsConfig = {
+    guides: [{ type: 'doc', id: 'guides/power-bi', className: 'guide-menu-item guide-menu-item-power-bi' }],
+};
+
+export default sidebar.guides;
